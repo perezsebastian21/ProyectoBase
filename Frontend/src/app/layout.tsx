57be50ff6d1540d36711df6c982dfe7d5bab239a
jcgarcia06 <gjuancruz65@gmail.com>
2026-06-04 15:14:07 -0300

    agrego splash home

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ClientAppWrapper from "@/components/ClientAppWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,7 +43,9 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ClientAppWrapper>{children}</ClientAppWrapper>
+      </body>
     </html>
   );
 }
